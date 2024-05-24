6abe1a354be0c34e6eec9159af42e135a83b7bf2
kelly_maass <kmaass77627@gmail.com>
2024-05-23 19:06:17 -0500

    complete

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const dodger = document.querySelector("#dodger");
 
+
+
+
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+   dodger.style.left = `${left - 1}px`;
   }
 }
 
 function moveDodgerRight() {
-  const rightNumbers = dodger.style.left.replace("px", "");
+ const rightNumbers = dodger.style.left.replace("px", "");
   const right = parseInt(rightNumbers, 10);
 
   if (rightNumbers < 360) {
-    dodger.style.left = `${right + 1}px`;
+   dodger.style.left = `${right + 1}px`;
   }
 }
 
